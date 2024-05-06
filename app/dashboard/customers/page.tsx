6a357cc1381d690c7a326003e8358b757dc616be
quangldt <quangldt@hivetech.vn>
2024-05-06 16:47:17 +0700

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+const CustomerPage = () => {
+  return (
+    <div>
+      <h1>Customer Page</h1>
+      {/* Add your customer page content here */}
+    </div>
+  );
+};
+
+export default CustomerPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+const InvoicesPage = () => {
+  return (
+    <div>
+      <h1>Invoices Page</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default InvoicesPage;
