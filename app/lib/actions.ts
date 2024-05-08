7186103dd0298e41460726f61894ac2ad0117cea
quangldt <quangldt@hivetech.vn>
2024-05-08 17:09:53 +0700

    Add deleteInvoice function and update DeleteInvoice component

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,3 +54,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   invoiceSuccess();
 }
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
